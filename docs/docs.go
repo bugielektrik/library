8aae6377579ad39cc473a5756646b127b3aba929
Zhanat Rakhmet <rakhmet.zhanat@gmail.com>
2023-06-14 00:40:05 +0600

    refactor: add uuid generator

--- docs/docs.go
+++ docs/docs.go
@@ -10,16 +10,7 @@ const docTemplate = `{
     "info": {
         "description": "{{escape .Description}}",
         "title": "{{.Title}}",
-        "termsOfService": "http://swagger.io/terms/",
-        "contact": {
-            "name": "API Support",
-            "url": "http://www.swagger.io/support",
-            "email": "support@swagger.io"
-        },
-        "license": {
-            "name": "Apache 2.0",
-            "url": "http://www.apache.org/licenses/LICENSE-2.0.html"
-        },
+        "contact": {},
         "version": "{{.Version}}"
     },
     "host": "{{.Host}}",
@@ -824,12 +815,12 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "1.0",
+	Version:          "",
 	Host:             "",
 	BasePath:         "",
 	Schemes:          []string{},
-	Title:            "Swagger Example API",
-	Description:      "This is a sample server.",
+	Title:            "",
+	Description:      "",
 	InfoInstanceName: "swagger",
 	SwaggerTemplate:  docTemplate,
 	LeftDelim:        "{{",
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -50,18 +50,6 @@ func New(d Dependencies, configs ...Configuration) (h *Handler, err error) {
 	return
 }
 
-//	@title			Swagger Example API
-//	@version		1.0
-//	@description	This is a sample server.
-//	@termsOfService	http://swagger.io/terms/
-
-//	@contact.name	API Support
-//	@contact.url	http://www.swagger.io/support
-//	@contact.email	support@swagger.io
-
-//	@license.name	Apache 2.0
-//	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
-
 // WithHTTPHandler applies a http handler to the Handler
 func WithHTTPHandler() Configuration {
 	return func(h *Handler) (err error) {
