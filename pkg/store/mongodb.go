ad6a5b590c7facca40409f92d95b0f3294b37a66
Zhanat Rakhmet <rakhmet.zhanat@gmail.com>
2023-08-15 23:24:14 +0600

    refactor: added drivers and split stores

--- pkg/store/mongodb.go
+++ pkg/store/mongodb.go
@@ -1,3 +1,7 @@
 package store
 
+//import (
+//	_ "github.com/golang-migrate/migrate/v4/database/mongodb"
+//)
+
 // mongodb://username:password@localhost:27017/?retryWrites=true&w=majority&tls=false
